7b010f614c24cd62ab0fc5085625f0d2b78a4645
Sashag99 <sashag99@mail.ru>
2017-02-26 00:47:09 +0200

    27.0217

---
+++ main.js
@@ -0,0 +1,12 @@
+var btn_1 = document.getElementById('btn_1');
+btn_1.addEventListener('click',function(){
+  	alert('Мы с вами свяжемся ')
+});
+var btn_2 = document.getElementById('btn_2');
+btn_2.addEventListener('click',function(){
+  	prompt('Напишите свой номер')
+});
+var btn_3 = document.getElementById('btn_3');
+btn_3.addEventListener('click',function(){
+  alert('бесплатная  коучинг-сессия будет проведена 27.02.2017')
+});
